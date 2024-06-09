0082edc8f5da270cc5b1dc698e9a2c19651f445f
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-06-09 15:13:32 +0530

    asos website

--- asos/src/pages/Product.jsx
+++ asos/src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Card,Text, Image, CardBody, Stack} from '@chakra-ui/react'
+import { Card, Text, Image, CardBody, Stack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
@@ -8,16 +8,29 @@ function Product() {
   const [filteredProduct, setFilteredProduct] = useState([]);
   const [sort, setSort] = useState("");
   const [category, setCategory] = useState("");
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     fetchProduct();
   }, []);
 
   async function fetchProduct() {
-    let url = `https://mercurial-midnight-rainbow.glitch.me/women`;
-    let res = await axios.get(url);
-    setProduct(res.data);
-    setFilteredProduct(res.data); // Set filtered product to all products initially
+    try {
+      let url = `https://mercurial-midnight-rainbow.glitch.me/women`;
+      let res = await axios.get(url);
+      if (res.status!==200) {
+        throw new Error(`HTTP error:${res.status}`)
+      }
+
+      setProduct(res.data);
+      setFilteredProduct(res.data); // Set filtered product to all products initially
+
+    } catch (error) {
+      setError(error.message)
+      console.log("there is some error , its fetching the data:",error)
+    }
+    finally { setLoading(false) }
   }
 
   useEffect(() => {
@@ -45,87 +58,110 @@ function Product() {
   return (
     <>
 
-<div className="container  ">
-      <h1 className="text-3xl font-bold text-center mb-5">Topshop New In</h1>
-      <p className="text-center text-gray-600 mb-5">
-        Welcome to the new era of Topshop. Retaining its fashion authority and
-        the very best...
-      </p>
-      <div className="flex gap-8  items-center justify-center">
-        <div className="col-span-1">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="sort">
-            Sort
-          </label>
 
-          <select
-            className="shadow  border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="sort"
-            onChange={(e) => setSort(e.target.value)}
-          >
-            <option value="">Select Sort</option>
-            <option value="asc">Price Low to High</option>
-            <option value="decs">Price High to Low</option>
-            
-            
-          </select>
 
-        </div>
-        <div className="col-span-1">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="category"
-          >
-            Category
-          </label>
-          <select
-            className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="category"
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <option value="">Select Category</option>
-            <option value="top">Top</option>
-            <option value="shorts">Shorts</option>
-            <option value="shirt">Shirt</option>
-            <option value="jeans">Jeans</option>
-            <option value="all in one">All in one</option>
-            <option value="dresses">Dresses</option>
-            
-          </select>
+      <div className="container  ">
+        <h1 className="text-3xl font-bold text-center mb-5">Topshop New In</h1>
+        <p className="text-center text-gray-600 mb-5">
+          Welcome to the new era of Topshop. Retaining its fashion authority and
+          the very best...
+        </p>
+        <div className="flex gap-8  items-center justify-center">
+          <div className="col-span-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="sort">
+              Sort
+            </label>
+
+            <select
+              className="shadow  border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="sort"
+              onChange={(e) => setSort(e.target.value)}
+            >
+              <option value="">Select Sort</option>
+              <option value="asc">Price Low to High</option>
+              <option value="desc">Price High to Low</option>
+
+
+            </select>
+
+          </div>
+          <div className="col-span-1">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="category"
+            >
+              Category
+            </label>
+            <select
+              className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="category"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+
+          <option value="">Select Category</option>
+          <option value="top">Top</option>
+          <option value="shorts">Shorts</option>
+          <option value="shirt">Shirt</option>
+          <option value="jeans">Jeans</option>
+          <option value="all in one">All in one</option>
+          <option value="dresses">Dresses</option>
+
+            </select>
+          </div>
+
         </div>
 
       </div>
-     
-    </div>
- 
- 
-    <div className=' w-[70%] grid  grid-cols-2  lg:grid-cols-4 m-auto ' >
 
-    {filteredProduct.map((e,id)=>{
-        return(
-          
-     <Link to={`/productdetail/${e.id}`}>
-    <Card maxW='sm' w="90%" mt="18"  >
-    <CardBody>
-    <Image
-      src={e.img} alt=''  h="350px"  />
-    <Stack>
-      <Text>{e.title}</Text>
-      <Text fontWeight="bold">Price: {e.price}</Text>
-    </Stack>
-  </CardBody>
-    </Card>
-    </Link>       
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-red-500"></div>
+        </div>
+      ) : error ? (
+        <div className="text-red-500 font-bold">Error: {error}</div>
+      ) : (
 
-)
 
-})}
 
-</div>
-    
-    
+        <div className=' w-[70%] grid  grid-cols-2  lg:grid-cols-4 m-auto ' >
+
+          {filteredProduct.map((e, id) => {
+            return (
+
+              <Link to={`/productdetail/${e.id}`}>
+                <Card maxW='sm' w="90%" mt="18"  >
+                  <CardBody>
+                    <Image
+                      src={e.img} alt='' h="350px" />
+                    <Stack>
+                      <Text>{e.title}</Text>
+                      <Text fontWeight="bold">Price: {e.price}</Text>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              </Link>
+
+            )
+
+          })}
+
+        </div>
+
+
+      )
+
+
+
+
+      }
+
     </>
-    
+
+
   )
 }
 
 export default Product
+
+
+
